25c2306c3a55a6f929a312e85456b7649edfac63
Scrub <scott-donaldson@outlook.com>
2022-07-11 21:42:13 +0100

    title text and animation added

--- portfolio/src/components/sections/title.tsx
+++ portfolio/src/components/sections/title.tsx
@@ -1,9 +1,15 @@
+import personal from '../../info/personal'
 import '../css/title.css'
 
 const TitleSection = () => {
   return (
-    <div>
-      <h1>test1</h1>
+    <div id='container'>
+      <div id='title-box'>
+      <h1 className='typewriter-text'>
+        <span className='name-anim'>Hi, I'm {personal().name_short}.</span>
+        <span className='title-anim'>Full stack developer</span>
+      </h1>
+      </div>
     </div>
   )
 }
