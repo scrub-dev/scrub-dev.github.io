cbbd503b218f125da04c7b533fd71a0e8b7a78d5
Scrub <scott-donaldson@outlook.com>
2022-09-08 20:57:28 +0100

    Updated title section to include css shapes

--- portfolio/src/components/sections/title.tsx
+++ portfolio/src/components/sections/title.tsx
@@ -1,10 +1,16 @@
 import personal from '../../info/personal'
 import style from '../css/title.module.css'
+import shapes from '../css/shapes.module.css'
 
 export const TitleSection = () =>
   <div className={style.container}>
     <div>
-      <h1>
+        <div className={shapes.square_1}></div>
+        <div className={shapes.square_2}></div>
+        <div className={shapes.square_3}></div>
+        <div className={shapes.square_4}></div>
+        <div className={shapes.square_5}></div>
+      <h1 className={style.shadow}>
         <span className={style.hello_world}>Hello World!</span>
         <div className={style.typewriter_text}>
           <span className={style.name_anim}>I'm {personal().name_short}.</span>
