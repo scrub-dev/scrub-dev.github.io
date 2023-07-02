1c294b8543545fe1470aa6d0fb143de22d424103
Scrub <scott-donaldson@outlook.com>
2023-07-02 21:01:13 +0100

    Initial Nav Bar implementation

--- portfolio-website/tailwind.config.js
+++ portfolio-website/tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 export default {
   content: [
     "./index.html",
@@ -8,6 +11,25 @@ export default {
     fontFamily: {
       display: ['IBM Plex Mono', 'Menlo', 'monospace'],
       body: ['IBM Plex Mono', 'Menlo', 'monospace'],
+    },
+    colors: {
+      transparent: 'transparent',
+      black: colors.black,
+      white: colors.white,
+      purple: colors.purple,
+      green: colors.green
+    },
+    container: {
+      center: true,
+    },
+    keyframes: {
+      blink: {
+        '0%, 100%': {color: 'transparent'},
+        '50%': {color: '#FFFFFF'}
+      }
+    },
+    animation: {
+      blinking: 'blink 1s infinite'
     }
   },
   plugins: [],
