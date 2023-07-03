fb8987bdac9948adb787e7f68a204bf26aed0ab5
Scrub <scott-donaldson@outlook.com>
2023-07-03 17:00:12 +0100

    project list sorted

--- portfolio-website/src/components/projects/projectCard.tsx
+++ portfolio-website/src/components/projects/projectCard.tsx
@@ -1,5 +1,30 @@
 import { FC } from "react"
+import { TProject } from "../../types"
 
-export const ProjectCard: FC = () => {
-    return (<></>)
+export const ProjectCard: FC<TProject> = (props) => {
+    return (
+    <div className="m-2 p-2 rounded-lg shadow hover:-translate-y-1 duration-300 bg-slate-700">
+        <div>
+            <span className=" text-white font-bold">&#123;<br></br></span>
+            <span className=" text-white">"name":</span>
+            <span className="text-white font-semibold underline">"{props.name}",</span>
+        </div>
+        <div>
+            <span className=" text-white">"desc":</span>
+            <span className="text-white">"{props.desc}",</span>
+        </div>
+        <div className="">
+            <span className="text-white ">"tech":</span>
+            <span className="text-white">&#91;{props.tech.map(e => `"${e}"`).join(",")}&#93;</span>
+        </div>
+        <div>
+            <span className="text-white">"links": </span>
+            <span className="text-white">&#123;</span>
+            {(props.gh_link) ? <span className="text-white"> "<a href={props.gh_link} target="_blank" className="underline">github</a>" </span>: ""}
+            {(props.prod_link) ? <span className="text-white">, "<a href={props.prod_link} target="_blank" className="underline">live</a>" </span>: ""}
+
+            <span className="text-white">&#125;</span>
+        </div>
+        <span className="text-white font-bold">&#125;</span>
+    </div>)
 }
--- portfolio-website/src/components/projects/projects.tsx
+++ portfolio-website/src/components/projects/projects.tsx
@@ -1,5 +1,10 @@
 import { FC } from "react"
+import getProjects from "../../utils/getProjects"
+import { ProjectCard } from "./projectCard"
 
 export const Projects: FC = () => {
-    return (<></>)
+    const projects = getProjects()
+    return (<>
+        {projects.map((e,i) => <ProjectCard {...e} key={""+i + e.name}/>)}
+    </>)
 }
