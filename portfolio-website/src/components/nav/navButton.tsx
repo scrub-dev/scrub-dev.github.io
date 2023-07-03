76cb1b97219c666dd1b88a6fd47b14510b1d1234
Scrub <scott-donaldson@outlook.com>
2023-07-03 16:59:56 +0100

    updated nav

--- portfolio-website/src/components/nav/navButton.tsx
+++ portfolio-website/src/components/nav/navButton.tsx
@@ -5,5 +5,5 @@ export const NavButton:FC<TNavEntry> = (props) => {
     let link: any = (props.link) ? props.link : (props.location) ? props.location : null
 
     return (<a className="px-1 underline underline-offset-4 hover:text-black hover:bg-white"
-    href={link}>{props.name}</a>)
+    href={link} target={props.link ? "_blank" : ""}>{props.name}</a>)
 }
