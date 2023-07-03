803e2b642fb94fd57de00d287487874e7bb85e26
Scrub <scott-donaldson@outlook.com>
2023-07-03 18:59:07 +0100

    mobie content panels

--- portfolio-website/src/components/panels/homePanel.tsx
+++ portfolio-website/src/components/panels/homePanel.tsx
@@ -2,8 +2,8 @@ import { FC } from "react"
 import me from '../../data/person.json'
 export const HomePanel: FC = () => {
     return (
-        <div id="home" className="h-screen flex justify-center bg-slate-950">
-            <div className="container pt-[20%] px-[10%] mx-auto">
+        <div id="home" className="min-h-screen flex justify-center bg-slate-950 ">
+            <div className="container py-[40%] md:py-[20%] px-[10%]">
                 <p className="text-3xl"><span className="text-green-400">$&gt;</span><span className="text-white"> Hi, I'm {me.name_short}. 👋 </span></p>
                 <p className="text-3xl"><span className="text-green-400">$&gt;</span><span className="text-white"> Welcome to my portfolio.</span></p>
                 <p className="text-3xl"><span className="text-green-400">$&gt;</span><span className="text-white"> Feel free to have a look around</span></p>
--- portfolio-website/src/components/panels/informationPanel.tsx
+++ portfolio-website/src/components/panels/informationPanel.tsx
@@ -9,14 +9,14 @@ export const InfoPanel: FC = () => {
     My current focus surrounds full stack development and competitive programming.
     `
     return (
-    <div id="info" className="h-screen flex justify-center bg-slate-900">
-        <div className="container pt-[20%] px-[10%] mx-auto">
+    <div id="info" className="min-h-screen flex justify-center bg-slate-900">
+        <div className="container py-[30%] md:py-[10%] px-[10%] mx-auto">
         <p className="text-3xl"><span className="text-green-400">$&gt;</span><span className="text-white"> cat about-me.txt</span></p><br></br>
         <p className="text-white text-3xl pl-10">{text}</p><br></br>
         <p className="text-3xl"><span className="text-green-400">$&gt;</span><span className="text-white"> cat *-link.svg</span></p>
-        <div className="py-5 pl-10 grid grid-flow-row gap-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {me.links.map(e => <span className="flex"><a target="_blank" href={e.url}>{e.icon ? <img src={e.icon} className="scale-80"></img> : e.name}</a></span>)}
-            <span><a target="_blank" href={"mailto: " + me.email}><img src="/assets/email.svg" className="scale-80"></img></a></span>
+        <div className="py-5 pl-10 grid grid-flow-row gap-2 grid-cols-4">
+            {me.links.map(e => <span className="flex"><a target="_blank" href={e.url}>{e.icon ? <img src={e.icon} className="scale-50"></img> : e.name}</a></span>)}
+            <span><a target="_blank" href={"mailto: " + me.email}><img src="/assets/email.svg" className="scale-50"></img></a></span>
         </div>
         <p className="text-3xl"><span className="text-green-400">$&gt;</span><span className="text-white animate-blinking"> _</span></p>
         </div>
