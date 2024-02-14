d7be81e9830e8e1d7d120daad00f0e228ab24f74
Scrub <43659841+Scott-Donaldson@users.noreply.github.com>
2024-02-14 23:36:05 +0000

    including the 404

--- scrub-dev/vite.config.ts
+++ scrub-dev/vite.config.ts
@@ -3,10 +3,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from "vite-plugin-svgr";
+import {resolve} from 'node:path'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "./"
+  base: "./",
+  build: {
+    rollupOptions : {
+      input: {
+        main: "./index.html",
+        fourohfour: "./404.html"
+      }
+    }
+  }
 })
