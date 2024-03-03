ae2065c241f58406e61f8d67290701a7afd7a069
Scrub <43659841+Scott-Donaldson@users.noreply.github.com>
2024-03-03 18:00:02 +0000

    Phrasing and updated CV Link

--- scrub-dev/src/components/about-me/intro.tsx
+++ scrub-dev/src/components/about-me/intro.tsx
@@ -7,7 +7,7 @@ export default () => {
         <span>
             <p className='text-white text-2xl'>Hello, my name is...</p>
             <p className='text-secondary-1 text-4xl lg:text-7xl'><strong>{capitalise(personal_data.name.long, {type: "SENTENCE"})}.</strong></p>
-            <p className='text-secondary-2 text-2xl lg:text-4xl'>Full Stack Software Engineer</p>
+            <p className='text-secondary-2 text-2xl lg:text-4xl'>Full Stack Software Developer</p>
         </span>
     </div>)
 }
